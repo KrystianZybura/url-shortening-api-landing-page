4e6ca994f63db10de1cf6a1dbeca2d438cd15fe7
Krystian Żybura <Krystian12324444@gmail.com>
2023-12-18 21:40:22 +0100

    GlobalStyle added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Normalize } from "styled-normalize";
+import { GlobalStyle } from "./GlobalStyle.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Normalize />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );

eff9317e06746dc0ee5b0fa116dec7bc0499a41a
Krystian Żybura <Krystian12324444@gmail.com>
2023-12-18 15:33:37 +0100

    global styles added

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const styled = { createGlobalStyle };
+
+export const GlobalStyle = styled.createGlobalStyle`
+  #root {
+    font-family: "Poppins", sans-serif;
+  }
+`;

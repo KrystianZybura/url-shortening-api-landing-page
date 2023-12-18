f3435018d6ec6d3ace23664e55e08d4b75cb7c47
Krystian Żybura <Krystian12324444@gmail.com>
2023-12-18 21:42:23 +0100

    test commit (temporary changes for testing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Navigation from "./components/Navigation";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Navigation />
+      <div className="container">
+        <div className="row justify-content-center align-items-center g-2">
+          <div className="col">Column</div>
+          <div className="col">Column</div>
+          <div className="col">Column</div>
+          <div className="col">Column</div>
+          <div className="col">Column</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;

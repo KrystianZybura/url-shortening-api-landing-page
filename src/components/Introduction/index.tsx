f256eb6f26c8ca3110b982600ed2a43c7835e4ae
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2023-12-27 19:58:12 +0100

    working illustration added

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,10 +1,25 @@
+import workingImage from "../../assets/illustration-working.svg";
+
 const Introduction = () => (
   <div className="div my-5">
-    <h1 className="fw-bold">More than just shorter links</h1>
-    <p className="m-0">
-      Build your brand's recognition and get detailed insights on how your links
-      are performing.
-    </p>
+    <div className="row">
+      <div className="col">
+        <h1 className="fw-bold">More than just shorter links</h1>
+        <p className="m-0">
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+      </div>
+      <div className="col">
+        <div>
+          <img
+            className="w-100 h-100"
+            src={workingImage}
+            alt="working illustration"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 );
 

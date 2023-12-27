3ac2d6e43ebd37e0c5b5f3ea4725a6239b896a7e
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2023-12-27 20:04:18 +0100

    theme added

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalStyle } from "./GlobalStyle.ts";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

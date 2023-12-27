c6ed657183d6e4ebc935c8e230c75640172d3e23
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2023-12-27 20:04:33 +0100

    global font size and color set

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -5,5 +5,7 @@ const styled = { createGlobalStyle };
 export const GlobalStyle = styled.createGlobalStyle`
   #root {
     font-family: "Poppins", sans-serif;
+    font-size: 18px;
+    color: ${({ theme }) => theme.colors.gray};
   }
 `;

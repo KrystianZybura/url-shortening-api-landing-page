5687f66385d9cadfd0914f14fb19bfd10e4d9903
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-01 19:53:20 +0100

    Output design improved

--- src/components/Output/index.tsx
+++ src/components/Output/index.tsx
@@ -1,13 +1,20 @@
 import Button from "../Button";
+import { ShortenedLink } from "./styled";
 
 const Output = () => (
   <section className="my-5">
     <ul className="list-group gap-4">
       <li className="list-group-item py-3 rounded-3">
-        <div className="row gap-3 container">
-          <span className="col-6 my-auto">Orignial link content</span>
-          <span className="col-4 my-auto">Shortened link</span>
-          <Button classes="col my-auto" content={`Copy`} />
+        <div className="row gap-3 px-2">
+          <span className="col-md-6 my-auto">
+            https://github.com/KrystianZybura/Personal-Homepage
+          </span>
+          <div className="col-md d-flex flex-column flex-md-row gap-3 text-end">
+            <ShortenedLink className="my-auto flex-grow-1">
+              https://shorturl.at/xyST6
+            </ShortenedLink>
+            <Button classes="px-4" content={`Copy`} />
+          </div>
         </div>
       </li>
     </ul>

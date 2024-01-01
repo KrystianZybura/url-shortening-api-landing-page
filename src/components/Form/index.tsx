0529b68f5ff65eee76ce784c80272f191d3c68f6
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-01 19:10:36 +0100

    form mobile design improved

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -11,18 +11,18 @@ const Form = () => {
 
   return (
     <StyledForm onSubmit={onFormSubmit} className="rounded-3">
-      <div className="row">
-        <div className="col-8 col-lg-10">
+      <div className="row gap-2 gap-md-0">
+        <div className="col-md-8 col-lg-10">
           <Input
             value={linkValue}
             onChange={({ target }) => setLinkValue(target.value)}
             placeholder="Shorten a link here.."
             type="text"
-            className="form-control py-3"
+            className="form-control py-2 py-md-3"
           />
         </div>
-        <div className="col-4 col-lg-2">
-          <Button classes="w-100 h-100" content="Shorten it!" />
+        <div className="col-md-4 col-lg-2">
+          <Button classes="w-100 h-100 py-2" content="Shorten it!" />
         </div>
       </div>
     </StyledForm>

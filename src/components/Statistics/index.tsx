8e90996298d3e7f8db536582dc5f82f417a8952b
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-02 20:38:12 +0100

    heading added

---
+++ src/components/Statistics/index.tsx
@@ -0,0 +1,13 @@
+const Statistics = () => (
+  <section className="">
+    <div className="text-center my-5 pt-5 w-50 mx-auto">
+      <h2 className="text-dark fw-bold">Advanced Statistics</h2>
+      <p>
+        Track how your links are performing across the web with our advanced
+        statistics dashboard.
+      </p>
+    </div>
+  </section>
+);
+
+export default Statistics;

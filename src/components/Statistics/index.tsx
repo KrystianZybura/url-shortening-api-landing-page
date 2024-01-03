b5703660b10fc019945e548d258adac3f41d16a7
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-03 15:14:08 +0100

    Boxes content added with an initial design

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,11 +1,30 @@
+import { Box } from "./styled";
+import {
+  brandRecognitionText,
+  detailedRecordsText,
+  fullyCustomizableText,
+  headingText,
+} from "./text";
+
 const Statistics = () => (
   <section className="">
     <div className="text-center my-5 pt-5 w-50 mx-auto">
       <h2 className="text-dark fw-bold">Advanced Statistics</h2>
-      <p>
-        Track how your links are performing across the web with our advanced
-        statistics dashboard.
-      </p>
+      <p>{headingText}</p>
+    </div>
+    <div className="row gap-3">
+      <Box className="col bg-white p-4 rounded-2">
+        <h3 className="fw-bold text-dark fs-5 pb-2 mt-5">Brand Recongnition</h3>
+        <p>{brandRecognitionText}</p>
+      </Box>
+      <Box className="col bg-white p-4 rounded-2">
+        <h3 className="fw-bold text-dark fs-5 pb-2 mt-5">Detailed Records</h3>
+        <p>{detailedRecordsText}</p>
+      </Box>
+      <Box className="col bg-white p-4 rounded-2">
+        <h3 className="fw-bold text-dark fs-5 pb-2 mt-5">Fully Customizable</h3>
+        <p>{fullyCustomizableText}</p>
+      </Box>
     </div>
   </section>
 );

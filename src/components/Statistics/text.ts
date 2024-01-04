e8a83772e690d481a074c1d0ed7cda3005f356a4
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-04 22:02:35 +0100

    minor correction for brandRecognitionText

--- src/components/Statistics/text.ts
+++ src/components/Statistics/text.ts
@@ -2,7 +2,7 @@ export const headingText =
   "Track how your links are performing across the web with our advanced statistics dashboard.";
 
 export const brandRecognitionText =
-  "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content";
+  "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content.";
 
 export const detailedRecordsText =
   "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.";

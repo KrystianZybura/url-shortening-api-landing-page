6cea0009f85dc85e8f4773aee6823f941e698d58
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-09 16:17:09 +0100

    App moved to components folder

---
+++ src/components/App.tsx
@@ -0,0 +1,30 @@
+import Aside from "./Aside";
+import Footer from "./Footer";
+import Form from "./Form";
+import Introduction from "./Introduction";
+import Navigation from "./Navigation";
+import Output from "./Output";
+import Statistics from "./Statistics";
+import { Container } from "./styled";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Introduction />
+        <Form />
+        <Container className="py-5">
+          <Output />
+          <Statistics />
+        </Container>
+        <Aside />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;

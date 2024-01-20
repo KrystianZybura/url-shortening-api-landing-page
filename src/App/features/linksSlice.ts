2f73df765ab2f673f8cef6ec8d2056e60ecd4b07
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-20 16:53:39 +0100

    select links added (without type assignment for now)

--- src/App/features/linksSlice.ts
+++ src/App/features/linksSlice.ts
@@ -3,7 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 const linksSlice = createSlice({
   name: "links",
   initialState: {
-    links: [],
+    links: [
+      {
+        defaultLink: "https://github.com/KrystianZybura/Personal-Homepage",
+        shortenedLink: "https://shorturl.at/xyST6",
+      },
+    ],
   },
   reducers: {
     addLink: ({ links }, { payload }) => {
@@ -13,5 +18,5 @@ const linksSlice = createSlice({
 });
 
 export const { addLink } = linksSlice.actions;
-
+export const selectLinks = (state) => state.links;
 export default linksSlice.reducer;

6f52d6d56c013b81719e2c9bf918fa3a96045202
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-20 16:56:03 +0100

    added missing types

--- src/App/components/Output/index.tsx
+++ src/App/components/Output/index.tsx
@@ -9,19 +9,27 @@ const Output = () => {
   return (
     <section className="container">
       <ul className="list-group gap-4">
-        {links.map(({ defaultLink, shortenedLink }) => (
-          <li className="list-group-item py-3 rounded-3">
-            <div className="row gap-3 px-2">
-              <span className="col-lg-6 my-auto">{defaultLink}</span>
-              <div className="col-lg d-flex flex-column flex-lg-row gap-3 text-lg-end">
-                <ShortenedLink className="my-auto flex-grow-1">
-                  {shortenedLink}
-                </ShortenedLink>
-                <Button classes="px-4" content={`Copy`} />
+        {links.map(
+          ({
+            defaultLink,
+            shortenedLink,
+          }: {
+            defaultLink: string;
+            shortenedLink: string;
+          }) => (
+            <li className="list-group-item py-3 rounded-3">
+              <div className="row gap-3 px-2">
+                <span className="col-lg-6 my-auto">{defaultLink}</span>
+                <div className="col-lg d-flex flex-column flex-lg-row gap-3 text-lg-end">
+                  <ShortenedLink className="my-auto flex-grow-1">
+                    {shortenedLink}
+                  </ShortenedLink>
+                  <Button classes="px-4" content={`Copy`} />
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          )
+        )}
       </ul>
     </section>
   );

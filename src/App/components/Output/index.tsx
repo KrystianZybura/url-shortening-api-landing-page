aa74e39f2c19816cd068d37dafb5aad33e675c87
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2024-01-20 16:53:51 +0100

    test output added

--- src/App/components/Output/index.tsx
+++ src/App/components/Output/index.tsx
@@ -1,24 +1,29 @@
+import { selectLinks } from "../../features/linksSlice";
 import Button from "../Button";
 import { ShortenedLink } from "./styled";
+import { useSelector } from "react-redux";
 
-const Output = () => (
-  <section className="container">
-    <ul className="list-group gap-4">
-      <li className="list-group-item py-3 rounded-3">
-        <div className="row gap-3 px-2">
-          <span className="col-lg-6 my-auto">
-            https://github.com/KrystianZybura/Personal-Homepage
-          </span>
-          <div className="col-lg d-flex flex-column flex-lg-row gap-3 text-lg-end">
-            <ShortenedLink className="my-auto flex-grow-1">
-              https://shorturl.at/xyST6
-            </ShortenedLink>
-            <Button classes="px-4" content={`Copy`} />
-          </div>
-        </div>
-      </li>
-    </ul>
-  </section>
-);
+const Output = () => {
+  const { links } = useSelector(selectLinks);
 
+  return (
+    <section className="container">
+      <ul className="list-group gap-4">
+        {links.map(({ defaultLink, shortenedLink }) => (
+          <li className="list-group-item py-3 rounded-3">
+            <div className="row gap-3 px-2">
+              <span className="col-lg-6 my-auto">{defaultLink}</span>
+              <div className="col-lg d-flex flex-column flex-lg-row gap-3 text-lg-end">
+                <ShortenedLink className="my-auto flex-grow-1">
+                  {shortenedLink}
+                </ShortenedLink>
+                <Button classes="px-4" content={`Copy`} />
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 export default Output;
